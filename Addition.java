e8a44f94cd00f8ac53bc2a5bd6f174fbcde615e3
chloeredders <101562087+chloeredders@users.noreply.github.com>
2022-03-14 11:17:19 +0000

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,19 @@
+class Addition
+//
+// A Simple program that adds two numbers entered from the keyboard
+// 
+{  
+      public static void main(String args[])
+        {
+  	int number1;
+	int number2;
+	int answer;
+
+	System.out.print("Enter the first number ->  ");
+        	number1 = EasyIn.getInt();
+        	System.out.print("Enter the second number ->  ");
+        	number2 = EasyIn.getInt();
+        	answer = number1 + number2;
+        	System.out.println("The sum of the two numbers is " + answer);
+        }
+}
